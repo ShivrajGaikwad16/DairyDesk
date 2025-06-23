bb7621658a480d1ac7fad0817d7c711580829895
git config --global u ser.name Rohan-Suryawanshi <suryawanshi9673r@gmail.com>
2025-06-23 11:40:07 +0530

    feat: Add react-router-dom dependency and clean up vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,26 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
    plugins: [react(), tailwindcss()],
-   build: {
-      rollupOptions: {
-         // Externalize Node.js core modules (if accidentally bundled)
-         external: ["fs", "path", "os", "stream", "crypto", "util", "buffer"],
-         output: {
-            manualChunks(id) {
-               if (id.includes("node_modules")) {
-                  return id
-                     .toString()
-                     .split("node_modules/")[1]
-                     .split("/")[0]
-                     .toString();
-               }
-            },
-         },
-      },
-      chunkSizeWarningLimit: 1000, // Optional: increase default limit from 500kB
-   },
 });
