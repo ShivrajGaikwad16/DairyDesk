ad7ae3e7ed52dad081da3851f01c1205f37e0ba5
git config --global u ser.name Rohan-Suryawanshi <suryawanshi9673r@gmail.com>
2025-06-23 11:10:23 +0530

    feat: Integrate Tailwind CSS into Vite configuration

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
+   plugins: [react()],
+   build: {
+      rollupOptions: {
+         // Externalize Node.js core modules (if accidentally bundled)
+         external: ["fs", "path", "os", "stream", "crypto", "util", "buffer"],
+         output: {
+            manualChunks(id) {
+               if (id.includes("node_modules")) {
+                  return id
+                     .toString()
+                     .split("node_modules/")[1]
+                     .split("/")[0]
+                     .toString();
+               }
+            },
+         },
+      },
+      chunkSizeWarningLimit: 1000, // Optional: increase default limit from 500kB
+   },
 });
