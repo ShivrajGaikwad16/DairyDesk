c4e36959364725e4bc7820895dcf63e1ca325d00
Rohan Surywanshi <rohan.suryawanshi@wargstech.com>
2025-09-28 14:37:15 +0530

    Add the vercel in the code

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AllRoutes from "./routes/AllRoutes";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
    return (
       <BrowserRouter>
          <AllRoutes />
+         <SpeedInsights/>
       </BrowserRouter>
    );
 }
